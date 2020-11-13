8df60f17b501e244b4fb70cbab78d3b488ec2e64
unknown <yeehawbw@gmail.com>
2020-11-13 12:13:24 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  const newsFeed = document.querySelector('.newsfeed');
+  window.jsonData.contentlets.forEach((articleData) => {
+    const article = document.createElement('article');
+    article.classList.add('media')
+    const thumbnail = document.createElement('i');
+    thumbnail.classList.add('far', 'fa-newspaper', 'fa-3x', 'mr-3');
+    const mediaBody = document.createElement('div');
+    mediaBody.classList.add('media-body')
+    const titleElement = document.createElement('h5');
+    titleElement.classList.add('mt-0')
+    titleElement.textContent = articleData.title;
+    const bodyElement = document.createElement('p');
+    bodyElement.innerHTML = articleData.NewsBody.slice(0, 200) + " ..."
+    const dateElement = document.createElement('p');
+    const date = new Date(articleData.NewsPublishDate);
+    dateElement.textContent = date.getMonth() + " / " + date.getDate() + " / " + date.getFullYear();
+    dateElement.classList.add('news-date')
+    article.appendChild(thumbnail);
+    article.appendChild(mediaBody);
+    mediaBody.appendChild(titleElement)
+    mediaBody.appendChild(bodyElement)
+    mediaBody.appendChild(dateElement)
+    newsFeed.appendChild(article);
+  })
+}
